e3cdebdc8a1efe836601dcd3863856fecf1c8308
yingtao <guoyingtao@outlook.com>
2018-11-03 14:47:32 -0400

    Add Mantis and Config files

--- Mantis/Mantis.swift
+++ Mantis/Mantis.swift
@@ -6,4 +6,18 @@
 //  Copyright © 2018 Echo. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+public struct Mantis {
+    
+    static var config = Config()
+    
+    static public func buildCropViewController(image: UIImage) -> CropViewController {
+        return CropViewController(image: image)
+    }
+    
+    static public func buildCropView(image: UIImage) -> CropView {
+        return CropView(image: image)
+    }
+}
+
