6589c6c543e98fd918977f8c8e36368a66238f50
yingtao <guoyingtao@outlook.com>
2018-11-08 21:17:58 -0500

    Refactor project to workspace

--- Mantis.podspec
+++ Mantis.podspec
@@ -10,7 +10,7 @@
 
 Pod::Spec.new do |s|
   s.name         = "Mantis"
-  s.version      = "0.1"
+  s.version      = "0.11"
   s.summary      = "A swift photo cropping tool which mimics Photo.app"
 
   s.description  = <<-DESC
---
+++ Mantis/Mantis/Mantis.h
@@ -0,0 +1,19 @@
+//
+//  Mantis.h
+//  Mantis
+//
+//  Created by Echo on 11/8/18.
+//  Copyright © 2018 Echo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Mantis.
+FOUNDATION_EXPORT double MantisVersionNumber;
+
+//! Project version string for Mantis.
+FOUNDATION_EXPORT const unsigned char MantisVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Mantis/PublicHeader.h>
+
+
