af8a81736e7d652fc5f43df3afa6af29d113192a
yingtao <guoyingtao@outlook.com>
2020-02-29 23:19:12 -0800

    Change version to 0.60

--- Tests/MantisTests/MantisTests.swift
+++ Tests/MantisTests/MantisTests.swift
@@ -3,10 +3,6 @@ import XCTest
 
 final class MantisTests: XCTestCase {
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Mantis().text, "Hello, World!")
     }
 
     static var allTests = [
