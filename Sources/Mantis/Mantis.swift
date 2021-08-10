2f335b394ce91c9a05f89de5232b631580e88b31
Aleksandr Nikolajev <aleksandr@mooncascade.com>
2021-08-10 13:39:44 +0300

    Add option to use custom localization tables & bundles

--- Sources/Mantis/Mantis.swift
+++ Sources/Mantis/Mantis.swift
@@ -28,6 +28,8 @@ private(set) var bundle: Bundle? = {
     return Mantis.Config.bundle
 } ()
 
+internal var localizationConfig = LocalizationConfig()
+
 public func cropViewController(image: UIImage,
                                config: Mantis.Config = Mantis.Config(),
                                cropToolbar: CropToolbarProtocol = CropToolbar(frame: CGRect.zero)) -> CropViewController {
@@ -84,7 +86,7 @@ public enum CropVisualEffectType {
 
 public enum CropShapeType {
     case rect
-    
+
     /**
       The ratio of the crop mask will always be 1:1.
      ### Notice
@@ -97,7 +99,7 @@ public enum CropShapeType {
      When maskOnly is true, the cropped image is kept rect
      */
     case ellipse(maskOnly: Bool = false)
-    
+
     /**
       The ratio of the crop mask will always be 1:1 and when maskOnly is true, the cropped image is kept rect.
      ### Notice
@@ -109,13 +111,13 @@ public enum CropShapeType {
      When maskOnly is true, the cropped image is kept rect
      */
     case roundedRect(radiusToShortSide: CGFloat, maskOnly: Bool = false)
-        
+
     case diamond(maskOnly: Bool = false)
-    
+
     case heart(maskOnly: Bool = false)
-    
+
     case polygon(sides: Int, offset: CGFloat = 0, maskOnly: Bool = false)
-    
+
     /**
       Each point should have normailzed values whose range is 0...1
      */
@@ -142,9 +144,14 @@ public struct CropToolbarConfig {
     public var fixRatiosShowType: FixRatiosShowType = .adaptive
     public var toolbarButtonOptions: ToolbarButtonOptions = .default
     public var presetRatiosButtonSelected = false
-    
+
     var mode: CropToolbarMode = .normal
     var includeFixedRatioSettingButton = true
+}
+
+public class LocalizationConfig {
+    public var bundle: Bundle? = Mantis.Config.bundle
+    public var tableName = "MantisLocalizable"
 }
 
 public struct Config {
@@ -155,28 +162,29 @@ public struct Config {
     public var presetFixedRatioType: PresetFixedRatioType = .canUseMultiplePresetFixedRatio()
     public var showRotationDial = true
     public var cropToolbarConfig = CropToolbarConfig()
-    
+    public private(set) var localizationConfig = Mantis.localizationConfig
+
     var customRatios: [(width: Int, height: Int)] = []
-    
+
     static private var bundleIdentifier: String = {
         return "com.echo.framework.Mantis"
     } ()
-    
+
     static private(set) var bundle: Bundle? = {
         guard let bundle = Bundle(identifier: bundleIdentifier) else {
             return nil
         }
-        
+
         if let url = bundle.url(forResource: "Resource", withExtension: "bundle") {
             let bundle = Bundle(url: url)
             return bundle
         }
         return nil
     } ()
-    
+
     public init() {
     }
-        
+
     mutating public func addCustomRatio(byHorizontalWidth width: Int, andHorizontalHeight height: Int) {
         customRatios.append((width, height))
     }
@@ -184,11 +192,11 @@ public struct Config {
     mutating public func addCustomRatio(byVerticalWidth width: Int, andVerticalHeight height: Int) {
         customRatios.append((height, width))
     }
-    
+
     func hasCustomRatios() -> Bool {
         return !customRatios.isEmpty
     }
-    
+
     func getCustomRatioItems() -> [RatioItemType] {
         return customRatios.map {
             (String("\($0.width):\($0.height)"), Double($0.width)/Double($0.height), String("\($0.height):\($0.width)"), Double($0.height)/Double($0.width))
