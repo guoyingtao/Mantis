ea58166158d5a43a0efb577601dcf4556c9b9add
Rick Shane <rshane@mac.com>
2024-03-22 15:58:16 +1100

    made private some variables in TransformStack

--- Sources/Mantis/Helpers/TransformStack.swift
+++ Sources/Mantis/Helpers/TransformStack.swift
@@ -7,15 +7,16 @@ class TransformStack: NSObject {
     
     weak var transformDelegate : TransformDelegate?
     
-    var transformAdjustmentsStack : Array = [TransformRecord]()
+    private var transformAdjustmentsStack: [TransformRecord] = []
     
-    var isRotated : Bool! = false
+    private var isRotated: Bool = false
     
-    var bottom : Int = 0
-    var top : Int = 0
+    private var bottom: Int = 0
     
-    var transformStackBottom : Int = 0
-    var transformStackTop : Int = 0
+    var top: Int = 0
+    
+    private var transformStackBottom: Int = 0
+    private var transformStackTop: Int = 0
         
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
