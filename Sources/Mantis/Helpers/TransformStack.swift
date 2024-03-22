49b3c1cf4059544c673767a3c82372a7ecd9b113
Rick Shane <rshane@mac.com>
2024-03-23 08:51:35 +1100

    Remove three unused variables from TransformStack

--- Sources/Mantis/Helpers/TransformStack.swift
+++ Sources/Mantis/Helpers/TransformStack.swift
@@ -9,13 +9,8 @@ class TransformStack: NSObject {
     
     private var transformAdjustmentsStack: [TransformRecord] = []
     
-    private var bottom: Int = 0
-    
     var top: Int = 0
     
-    private var transformStackBottom: Int = 0
-    private var transformStackTop: Int = 0
-        
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
@@ -45,7 +40,6 @@ class TransformStack: NSObject {
     func reset() {
         transformAdjustmentsStack.removeAll()
         top = 0
-        bottom = 0
     }
     
     @objc func undoStatusChanged(notification: NSNotification?) {
