a13728200981dc9800e9560a9913398fe33c988a
Rick Shane <rshane@mac.com>
2024-03-22 15:32:40 +1100

    - Fixed a localization bug - Removed a dead deprecation directive

--- Sources/Mantis/Protocols/CropViewControllerDelegate.swift
+++ Sources/Mantis/Protocols/CropViewControllerDelegate.swift
@@ -18,8 +18,6 @@ public protocol CropViewControllerDelegate: AnyObject {
     func cropViewControllerDidBeginResize(_ cropViewController: CropViewController)
     func cropViewControllerDidEndResize(_ cropViewController: CropViewController, original: UIImage, cropInfo: CropInfo)
     
-    @available(*, deprecated, message: "Use cropViewControllerDidImageTransformed(_ cropViewController: CropViewController, transformation: Transformation) instead")
-    
     func cropViewControllerDidImageTransformed(_ cropViewController: CropViewController, transformation: Transformation)
     
     func cropViewController(_ cropViewController: CropViewController, didBecomeResettable resettable: Bool)
