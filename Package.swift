941d4c45fa393a2e937e69f5d365d46743d5583c
ShikiSuen <shikisuen@outlook.com>
2025-05-30 21:45:43 +0800

    fix: Add macCatalyst as a supported platform in Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Mantis",
     defaultLocalization: "en",
-    platforms: [.iOS(.v12)],
+    platforms: [.iOS(.v12), .macCatalyst(.v13)],
     products: [
         .library(
             name: "Mantis",
